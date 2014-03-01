1a297f3fd719b83e5fc62608f1533e096625261f
antonbelev <belevanton@gmail.com>
2014-03-01 19:46:47 +0000

    server c handles images now

--- NS3_course_work/server.c
+++ NS3_course_work/server.c
@@ -87,7 +87,7 @@ static void parseRequest(char *buf, int fd)
 	printf("Parse Request starting...\n");
 	int content_type = 0;
 	//GET check part
-	char cwd[1024];// PROBLEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEM
+	char cwd[1024];
 	char subbuff[4];
 	memcpy(subbuff, buf, 3);
 	subbuff[3] = '\0';
@@ -109,15 +109,15 @@ static void parseRequest(char *buf, int fd)
 	
 	//printf("buff: %s\n", buf);
 	char *filenameStart = buf + 4;
-	printf("filenameLine: %s\n", filenameStart);
+	//printf("filenameLine: %s\n", filenameStart);
 	char *filenameEnd = strstr(filenameStart, " ");
-	printf("filenameEnd: %s\n", filenameEnd);
+	//printf("filenameEnd: %s\n", filenameEnd);
 	char *filename = malloc(sizeof(char) * (strlen(filenameStart) - strlen(filenameEnd) + 1));
 	
 	char *fileExtensionStart = strstr(filenameStart, ".");
 	char *fileExtension = malloc(sizeof(char) * (strlen(fileExtensionStart) - strlen(filenameEnd) + 1));
 	
-	printf("strlen(fileExtensionStart) - strlen(filenameEnd) = %d\n", strlen(fileExtensionStart) - strlen(filenameEnd));
+	//printf("strlen(fileExtensionStart) - strlen(filenameEnd) = %d\n", strlen(fileExtensionStart) - strlen(filenameEnd));
 	memcpy(fileExtension, fileExtensionStart, strlen(fileExtensionStart) - strlen(filenameEnd));
 	fileExtension[strlen(fileExtensionStart) - strlen(filenameEnd)] = '\0';
 	
@@ -128,26 +128,26 @@ static void parseRequest(char *buf, int fd)
 	else if (strcmp(fileExtension, ".jpeg") == 0)
 	  content_type = 1;
 	
-	printf("fileExtension: %s\n", fileExtension);
+	//printf("fileExtension: %s\n", fileExtension);
 	
-	printf("strlen(filenameStart) - strlen(filenameEnd) = %d\n", strlen(filenameStart) - strlen(filenameEnd));
+	//printf("strlen(filenameStart) - strlen(filenameEnd) = %d\n", strlen(filenameStart) - strlen(filenameEnd));
 	memcpy(filename, filenameStart, strlen(filenameStart) - strlen(filenameEnd));
 	filename[strlen(filenameStart) - strlen(filenameEnd)] = '\0';
 	
 	//char *endOfString = '\0';
 	//strcat(filename, endOfString);
 	
-	printf("filename: %s\n", filename);
+	//printf("filename: %s\n", filename);
 	strcat(cwd, filename);
 	
 	char *cwdfname = malloc(sizeof(char) * (strlen(cwd) + 1));	
 	memcpy(cwdfname, cwd, strlen(cwd));
 	cwdfname[strlen(cwd)] = '\0';
 	//strcat(cwdfname, endOfString);
-	printf("cwdfname before free: %s\n", cwdfname);
+	//printf("cwdfname before free: %s\n", cwdfname);
 	free(filename);
 	free(fileExtension);
-	printf("cwdfname after free: %s\n", cwdfname);
+	//printf("cwdfname after free: %s\n", cwdfname);
 		
 	//Host name check part
 	char *hostcur = strcasestr(buf, "host:");\
@@ -168,10 +168,10 @@ static void parseRequest(char *buf, int fd)
 	      memcpy(hostnamereq, hostnamereqStart, hostnamereqEnd - hostnamereqStart);      
 
 	      if (strcmp(hostnamereq, hostname) != 0){
-		printf("Host name doesn't match 1: hostnamereq %s, hostname %s\n", hostnamereq, hostname);
+		//printf("Host name doesn't match 1: hostnamereq %s, hostname %s\n", hostnamereq, hostname);
 		strcat(hostname, ".dcs.gla.ac.uk:1108\0");
 		if (strcmp(hostnamereq, hostname) != 0){
-		    printf("Host name doesn't match 2: hostnamereq %s, hostname %s\n", hostnamereq, hostname);
+		    //printf("Host name doesn't match 2: hostnamereq %s, hostname %s\n", hostnamereq, hostname);
 		    returnResponse(fd, 1, NULL, 0); //BAD REQUEST
 		} else {
 		  returnResponse(fd, 0, cwdfname, content_type);//OK
@@ -197,6 +197,9 @@ static char* returnResponse(int connfd, int error_type, char *filename, int cont
 	char *content_type_html = "Content-Type: text/html\r\n";	
 	char *content_type_jpeg = "Content-Type: image/jpeg\r\n";
 	char *content_type_gif = "Content-Type: image/gif\r\n";
+	 
+	char *content_length = "Content-Length: ";	
+	char *content_length_buff = NULL;
 	
 	char *connection_close = "Connection: close\r\n";
 	char *response_ok = "HTTP/1.1 200 OK\r\n";
@@ -207,21 +210,18 @@ static char* returnResponse(int connfd, int error_type, char *filename, int cont
 	char *okBUFF;
 	char *badBUFF = malloc(sizeof(char) * (strlen(response_bad) + strlen(content_type_html) + strlen(connection_close) + 1));
 	badBUFF[0] = '\0';
-	printf("badBUFF...\n");
 	strcat(badBUFF, response_bad);
 	strcat(badBUFF, content_type_html);
 	strcat(badBUFF, connection_close);
 	
 	char *nfBUFF = malloc(sizeof(char) * (strlen(response_not_found) + strlen(content_type_html) + strlen(connection_close)+  1));
 	nfBUFF[0] = '\0';
-	printf("nfBUFF...\n");
 	strcat(nfBUFF, response_not_found);
 	strcat(nfBUFF, content_type_html);
 	strcat(nfBUFF, connection_close);
 	
 	char *internalBUFF =  malloc(sizeof(char) * (strlen(response_internal_error) + strlen(content_type_html) + strlen(connection_close)+  1));
 	nfBUFF[0] = '\0';
-	printf("internalBUFF...\n");
 	strcat(internalBUFF, response_internal_error);
 	strcat(internalBUFF, content_type_html);
 	strcat(internalBUFF, connection_close);
@@ -229,7 +229,6 @@ static char* returnResponse(int connfd, int error_type, char *filename, int cont
 	if (content_type == 0){
 	    okBUFF = malloc(sizeof(char) * (strlen(response_ok) + strlen(content_type_html) + strlen(connection_close) + 1));
 	    okBUFF[0] = '\0';
-	    printf("okBUFF...\n");
 	    strcat(okBUFF, response_ok);
 	    strcat(okBUFF, content_type_html);
 	    strcat(okBUFF, connection_close);
@@ -251,18 +250,30 @@ static char* returnResponse(int connfd, int error_type, char *filename, int cont
 	
 	char *outputBUFF;
 	char *source = NULL;
+	char *fopenMode;
 	
+	
+	if ( content_type == 1 || content_type == 2)
+	    fopenMode = "rb";
+	else
+	    fopenMode = "r";
+	
+	//printf("filename: %s, content_type = %d, fopenMode = %s\n", filename, content_type, fopenMode);
+	
+	long bufsize = 0;
+	long headerLen = 0;
 	if (filename != NULL)
 	    printf("filename response %s \n", filename);
 	if (error_type == 0) {
 	    FILE *file;
-	    if ((file = fopen(filename, "r")) == NULL) {
+	    if ((file = fopen(filename, fopenMode)) == NULL) {
 		  printf("File not found\n");
 		  returnResponse(connfd, 2, filename, 0); // NOT FOUND
 		  return;
 	    } else {
+		  size_t newLen;
 		  if (fseek(file, 0L, SEEK_END) == 0) {
-		      long bufsize = ftell(file);
+		      bufsize = ftell(file);
 		      if (bufsize == -1) { 
 			  returnResponse(connfd, 3, NULL, 0); // Internal Server error
 		      }
@@ -271,37 +282,55 @@ static char* returnResponse(int connfd, int error_type, char *filename, int cont
 		      if (fseek(file, 0L, SEEK_SET) != 0) { 
 			  returnResponse(connfd, 3, NULL, 0); // Internal Server error
 		      }
-		      size_t newLen = fread(source, sizeof(char), bufsize, file);
+		      newLen = fread(source, sizeof(char), bufsize, file);
 		      if (newLen == 0) {
 			  returnResponse(connfd, 3, NULL, 0); // Internal Server error
 		      } else {
 			  source[++newLen] = '\0';
 		      }
 		  }
-		  outputBUFF = malloc(sizeof(char) * (strlen(okBUFF) + strlen(source)+ 1));
+		  bufsize = newLen;
+		  printf("predi content_length malloca sum\n");
+		  content_length_buff = malloc(sizeof(char) * (strlen(content_length) + bufsize + 3));
+		  content_length_buff[0] = '\0';
+		  sprintf(content_length_buff, content_length);
+		  sprintf(content_length_buff + strlen(content_length_buff), "%d", bufsize);
+		  sprintf(content_length_buff, "\r\n");
+		  
+		  printf("content_length |%s|\n", content_length_buff);
+		  outputBUFF = malloc(sizeof(char) * (strlen(okBUFF) + strlen(content_length_buff) + bufsize + 1));
 		  outputBUFF[0] = '\0';
 		  strcat(outputBUFF, okBUFF);
-		  strcat(outputBUFF, source);
+		  printf("predi strcat na content_length_buff\n");
+		  strcat(outputBUFF, content_length_buff);
+		  headerLen = strlen(outputBUFF);
+		  memcpy(outputBUFF + headerLen, source, bufsize);
+		  printf("SOURCE \n%s\n", source);
 		  fclose(file);
 	    }	    
 	}
 	else if (error_type == 1){
 	    outputBUFF = malloc(sizeof(char) * (strlen(badBUFF) + 1));
 	    outputBUFF[0] = '\0';
+	    headerLen = strlen(badBUFF);
 	    strcat(outputBUFF, badBUFF);
 	}
 	else if (error_type == 2){
 	    outputBUFF = malloc(sizeof(char) * (strlen(nfBUFF) + 1));
 	    outputBUFF[0] = '\0';
+	    headerLen = strlen(nfBUFF);
 	    strcat(outputBUFF, nfBUFF);
 	}
 	else if (error_type == 3){
 	    outputBUFF = malloc(sizeof(char) * (strlen(nfBUFF) + 1));
 	    outputBUFF[0] = '\0';
+	    headerLen = strlen(nfBUFF);
 	    strcat(outputBUFF, nfBUFF);
 	}
 	
-	int datalen = strlen(outputBUFF);
+	
+	
+	int datalen = headerLen + bufsize;
 	int bytes_count;
 	if ((bytes_count = write(connfd, outputBUFF, datalen)) == -1) {
 	    printf("Server: Couldn't write the message. Bytes written: %d. Message length: %d.\n", bytes_count, datalen);
@@ -309,13 +338,14 @@ static char* returnResponse(int connfd, int error_type, char *filename, int cont
 	
 	outputBUFF[0]= '\0';
 	//free(outputBUFF);
+	if (content_length_buff != NULL)
+	    free(content_length_buff);
 	free(okBUFF);
 	free(badBUFF);
 	free(nfBUFF);
 	free(internalBUFF);
 	free(source);
 	free(filename);
-	
 }
  
   
